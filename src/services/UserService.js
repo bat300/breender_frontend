2cfd7b52e6da2a38eb2d8acee9eb93f2a03c5f8a
Alina Turbina <alina.turbina@tum.de>
2021-06-16 11:17:57 +0200

    change user login function, extract user directly

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,20 @@
-import HttpService from "./HttpService";
+import HttpService from './HttpService';
+import axios from 'axios';
 
 export default class UserService {
     static baseURL() {
-        return "http://localhost:4000/auth";
+        return 'http://localhost:4000/auth';
+    }
+
+    static extractUser(token) {
+        let base64Url = token.split('.')[1];
+        let base64 = base64Url.replace('-', '+').replace('_', '/');
+        let userJson = JSON.parse(atob(base64));
+        return {
+                id: userJson._id,
+                username: userJson.username,
+                role: userJson.role,
+        };
     }
 
     static register(user, pass, isAdmin) {
@@ -25,24 +37,26 @@ export default class UserService {
     }
 
     static login(user, pass) {
-        return new Promise((resolve, reject) => {
-            HttpService.post(
-                `${UserService.baseURL()}/login`,
-                {
-                    username: user,
-                    password: pass,
-                },
-                function (data) {
-                    resolve(data);
-                },
-                function (textStatus) {
-                    reject(textStatus);
+        return new Promise(async (resolve, reject) => { 
+            try {
+                const loginData = { username: user, password: pass };
+                const { data } = await axios.post(`/auth/login/`, loginData);
+                let userData = {};
+
+                if (data.hasOwnProperty('token')) {
+                    window.localStorage['jwtToken'] = data.token;
+                    userData = this.extractUser(data.token);
+                    console.log(userData);
                 }
-            );
+
+                resolve(userData);
+            } catch (err) {
+                reject(err);
+            }
         });
     }
 
     static logout() {
-        window.localStorage.removeItem("jwtToken");
+        localStorage.removeItem('jwtToken');
     }
 }
