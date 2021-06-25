6d65080301240461b0d97bb6569ef2892e513ab2
Nikol Nikolova <nikol.nikolova@tum.de>
2021-06-25 16:18:49 +0200

    Add icons to all PetInformationListItem

--- src/components/pet-profile/PetInformationListItem.js
+++ src/components/pet-profile/PetInformationListItem.js
@@ -1,50 +1,49 @@
-import React from "react";
-import {
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListItemAvatar,
-  Avatar,
-} from "@material-ui/core";
-import GetAppIcon from "@material-ui/icons/GetApp";
-import VerificationIcon from "../../components/VerificationIcon";
+import React from 'react';
+import { ListItem, ListItemIcon, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import PetsIcon from '@material-ui/icons/Pets';
+import VerificationIcon from '../../components/VerificationIcon';
 
 function PetInformationListItem(props) {
-  if (props.primary) {
-    return (
-      <ListItem
-        button={isButton(props.itemType)}
-        component={isButton(props.itemType) ? "a" : null}
-        href={props.itemType === "document" ? props.url : null}
-      >
-        <Icon itemType={props.itemType} avatar={props.avatar} />
-        <ListItemText primary={props.primary} secondary={props.secondary} />
-        {props.itemType === "document" && (
-          <VerificationIcon verified={props.verified} />
-        )}
-      </ListItem>
-    );
-  }
-  return null;
+    if (props.primary) {
+        return (
+            <ListItem button={isButton(props.itemType)} component={isButton(props.itemType) ? 'a' : null} href={props.itemType === 'document' ? props.url : null}>
+                <Icon itemType={props.itemType} avatar={props.avatar} />
+                <ListItemText primary={props.primary} secondary={props.secondary} />
+                {props.itemType === 'document' && <VerificationIcon verified={props.verified} />}
+            </ListItem>
+        );
+    }
+    return null;
 }
 
 function Icon(props) {
-  if (props.itemType === "owner") {
-    return (
-      <ListItemAvatar>
-        <Avatar src={props.avatar} />
-      </ListItemAvatar>
-    );
-  } else
-    return (
-      <ListItemIcon>
-        {props.itemType === "document" ? <GetAppIcon /> : null}
-      </ListItemIcon>
-    );
+    switch (props.itemType) {
+        case 'owner':
+            return (
+                <ListItemAvatar>
+                    <Avatar src={props.avatar} />
+                </ListItemAvatar>
+            );
+        case 'document':
+            return (
+                <ListItemIcon>
+                    <GetAppIcon />
+                </ListItemIcon>
+            );
+        default:
+            return (
+                <ListItemAvatar>
+                    <Avatar>
+                        <PetsIcon />
+                    </Avatar>
+                </ListItemAvatar>
+            );
+    }
 }
 
 function isButton(type) {
-  return type === "owner" || type === "document";
+    return type === 'owner' || type === 'document';
 }
 
 export default PetInformationListItem;
