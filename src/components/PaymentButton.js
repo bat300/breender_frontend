d18539d0cdbbdda69579d07bd1f46808c490639f
Nikol Nikolova <nikol.nikolova@tum.de>
2021-06-25 16:59:47 +0200

    Change color of buttons and competition cards

--- src/components/PaymentButton.js
+++ src/components/PaymentButton.js
@@ -1,31 +1,26 @@
-import React from "react";
-import { Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(3),
-    padding: theme.spacing(1),
-    paddingLeft: theme.spacing(6),
-    paddingRight: theme.spacing(6),
-  },
+    button: {
+        margin: theme.spacing(3),
+        padding: theme.spacing(1),
+        paddingLeft: theme.spacing(6),
+        paddingRight: theme.spacing(6),
+    },
 }));
 
 function PaymentButton(props) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  //TODO: The button needs to have payment funcitonality
-  return (
-    <Button
-      variant="contained"
-      color="primary"
-      className={classes.button}
-      endIcon={<ShoppingCartIcon />}
-    >
-      € {props.price}
-    </Button>
-  );
+    //TODO: The button needs to have payment funcitonality
+    return (
+        <Button variant="contained" color="secondary" className={classes.button} endIcon={<ShoppingCartIcon />}>
+            € {props.price}
+        </Button>
+    );
 }
 
 export default PaymentButton;
