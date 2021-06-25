9f6e4b7c349533c1661ceeb530fad772aef7f534
Alina Turbina <alina.turbina@tum.de>
2021-06-25 22:47:02 +0200

    apply separation between private routes with header and auth router without logged in user

--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,61 @@
-import MovieListView from './views/MovieListView';
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+// views
 import UserLoginView from './views/UserLoginView';
 import SignUpView from './views/SignUpView';
-import MovieDetailsView from './views/MovieDetailsView';
 import AddPetView from './views/AddPetView';
 import EmailConfirmationView from './views/EmailConfirmationView';
 import Search from './components/Search';
+// services
+import { LocalStorageService } from 'services';
+import Header from 'components/Header';
+import AppTheme from 'theming/themetypes';
 
-// routes within the movie database example app
 // used for routing
+export const PrivateRoute = (props) => {
+    // theme for app
+    const [theme, setTheme] = React.useState(AppTheme.LIGHT);
 
-const routes = [
-    {
-        path: '/',
-        component: Search,
-        exact: true,
-    },
-    {
-        path: '/login',
-        component: UserLoginView,
-    },
-    {
-        path: '/register',
-        component: SignUpView,
-    },
-    {
-        path: '/confirmation/:email/:token',
-        component: EmailConfirmationView,
-    },
-    {
-        path: '/movie/:id',
-        component: MovieDetailsView,
-    },
-    /** @TODO added for the test, maybe need to deleted later */
-    {
-        path: '/add-pet',
-        component: AddPetView,
-    },
-];
+    // toggle theme
+    const toggleTheme = () => {
+        setTheme(theme === AppTheme.LIGHT ? AppTheme.DARK : AppTheme.LIGHT);
+    };
 
-export default routes;
+    return LocalStorageService.isAuthorized() ? (
+        <Route {...props}>
+            <Header darkmode={theme === AppTheme.DARK} toggletheme={toggleTheme} />
+            {props.children}
+        </Route>
+    ) : (
+        <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+    );
+};
+
+export const AuthRoute = (props) => <Route {...props}>{props.children}</Route>;
+
+const Routes = () => {
+    return (
+        <Switch>
+            <AuthRoute exact path="/login">
+                <UserLoginView />
+            </AuthRoute>
+            <AuthRoute path="/register">
+                <SignUpView />
+            </AuthRoute>
+            <AuthRoute path="/confirmation/:email/:token">
+                <EmailConfirmationView />
+            </AuthRoute>
+            <PrivateRoute exact path="/">
+                <Search />
+            </PrivateRoute>
+            <PrivateRoute exact path="/pet/:id">
+                {null}
+            </PrivateRoute>
+            <PrivateRoute exact path="/add-pet">
+                <AddPetView />
+            </PrivateRoute>
+        </Switch>
+    );
+};
+
+export default Routes;
