7246d37cc9f78636c67fb19a17ff37821a7310be
Alina Turbina <alina.turbina@tum.de>
2021-06-27 21:43:14 +0200

    remove unused pet hooks

--- src/helper/hooks/pets.hooks.js
+++ src/helper/hooks/pets.hooks.js
@@ -2,10 +2,5 @@ import { useSelector } from 'react-redux';
 
 export const usePet = () => useSelector((state) => state.pets.pet);
 
-export const useProfilePicture = () => useSelector((state) => state.pets.profilePicture);
+export const useProfilePicture = () => useSelector((state) => state.pets.profilePictureToRemove);
 
-export const usePictures = () => useSelector((state) => state.pets.pictures);
-
-export const useDocuments = () => useSelector((state) => state.pets.documents);
-
-export const useCompetitions = () => useSelector((state) => state.pets.competitions);
