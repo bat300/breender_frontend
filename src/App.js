0a0ea97b3e93e1156a2432aa365ee65d314f91e5
Alina Turbina <alina.turbina@tum.de>
2021-06-27 18:22:04 +0200

    apply redux persist to the app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunkMiddleware from 'redux-thunk';
 import { MuiThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ScrollContainer from './components/ScrollContainer';
 
-import reducers from "./redux/reducers";
-import routes from "./routes";
-import Header from "./components/Header";
-import AppTheme from "./theming/themetypes";
-import AppThemeOptions from "./theming/themes";
-import AxiosConfiguration from './helper/axios'
+import reducers from './redux/reducers';
+import routes from './routes';
+import Header from './components/Header';
+import AppTheme from './theming/themetypes';
+import AppThemeOptions from './theming/themes';
+import AxiosConfiguration from './helper/axios';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const useStyles = makeStyles((theme) => ({
     appRoot: {
@@ -22,6 +25,11 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column',
     },
 }));
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
 function App() {
     const classes = useStyles();
@@ -34,7 +42,9 @@ function App() {
     }, []);
 
     // create store for redux
-    const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+    const persistedReducer = persistReducer(persistConfig, reducers);
+    const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+    const persistor = persistStore(store);
 
     // theme for app
     const [theme, setTheme] = React.useState(AppTheme.LIGHT);
@@ -48,17 +58,19 @@ function App() {
         <div className={classes.appRoot}>
             <MuiThemeProvider theme={createMuiTheme(AppThemeOptions[theme])}>
                 <Provider store={store}>
-                    <CssBaseline />
-                    <React.Fragment>
-                        <Header darkmode={theme === AppTheme.DARK} toggletheme={toggleTheme} />
-                        <ScrollContainer>
-                            <Switch>
-                                {routes.map((route, i) => (
-                                    <Route key={i} {...route} />
-                                ))}
-                            </Switch>
-                        </ScrollContainer>
-                    </React.Fragment>
+                    <PersistGate loading={null} persistor={persistor}>
+                        <CssBaseline />
+                        <React.Fragment>
+                            <Header darkmode={theme === AppTheme.DARK} toggletheme={toggleTheme} />
+                            <ScrollContainer>
+                                <Switch>
+                                    {routes.map((route, i) => (
+                                        <Route key={i} {...route} />
+                                    ))}
+                                </Switch>
+                            </ScrollContainer>
+                        </React.Fragment>
+                    </PersistGate>
                 </Provider>
             </MuiThemeProvider>
         </div>
