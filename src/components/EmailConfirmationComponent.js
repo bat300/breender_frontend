ef17692f7850b456af1c66cd978ed590b5850f6f
Oleksandra Talalaieva <oleksandra.talalaieva@tum.de>
2021-07-12 11:59:46 +0200

    fix router and center button

--- src/components/EmailConfirmationComponent.js
+++ src/components/EmailConfirmationComponent.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        position: 'absolute',
+        left: '50%',
+        top: '60%',
+        transform: 'translate(-50%, -50%)',
+    },
+}));
 
 /**
  * For user login
  * @param {props} props
  */
 function EmailConfirmationComponent(props) {
+    const classes = useStyles();
+
     return (
         <Grid container spacing={0} direction="column" alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
-            <Grid item xs={3}>
+            <Grid item xs={12}>
                 {props.confirmation.confirmation ? (
                     <div>
                         <h1>{props.confirmation.confirmation.message}</h1>
-                        <Button variant="contained" color="secondary" onClick={props.onMove}>
-                            Go to Home Page
-                        </Button>
+                        <div className={classes.button}>
+                            <Button variant="contained" color="secondary" onClick={props.onMove}>
+                                Go to Home Page
+                            </Button>
+                        </div>
                     </div>
                 ) : (
                     <div>
