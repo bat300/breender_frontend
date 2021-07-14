ef3156955478b1c7c50cda4f99862439aaddf7a9
Alina Turbina <alina.turbina@tum.de>
2021-07-14 15:52:10 +0200

    create redirections for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, IconButton, Link, Toolbar, Typography } from '@material-ui/core';
 
 import logo from '../images/breender.svg';
-import SearchIcon from '@material-ui/icons/Search';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PersonIcon from '@material-ui/icons/Person';
 import KebabMenu from './KebabMenu';
@@ -14,15 +13,27 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
     },
     title: {
-        flexGrow: 1,
         paddingLeft: theme.spacing(2),
+        cursor: 'pointer',
+        fontWeight: 'lighter',
+        '&:hover': {
+            color: 'inherit',
+            textDecoration: 'none',
+            fontWeight: 'normal',
+        },
     },
     title2: {
         paddingLeft: theme.spacing(2),
+        fontWeight: 'lighter',
     },
     logo: {
         flexGrow: 1,
         alignItems: 'flex-start',
+    },
+    navigation: {
+        display: 'flex',
+        flexGrow: 1,
+        alignItems: 'stretch',
     },
 }));
 
@@ -32,39 +43,51 @@ const useStyles = makeStyles((theme) => ({
  */
 function Header(props) {
     const classes = useStyles();
-
     const [menuAnchor, setMenuAnchor] = React.useState(null);
+    const [navigationSelectedStyle, setNavigationSelectedStyle] = React.useState({ search: null, blog: null, premium: null });
 
-    const onClickLogo = () => {
-        props.history.push('/');
-    };
+    useEffect(() => {
+        // @TODOs
+        switch (props.history.location.pathname) {
+            case '/':
+                setNavigationSelectedStyle({ search: { fontWeight: 'normal' }, blog: null, premium: null }); break;
+            case '/premium':
+                setNavigationSelectedStyle({ search: null, blog: null, premium: { fontWeight: 'normal' } }); break;
+            default:
+                setNavigationSelectedStyle({ search: null, blog: null, premium: null }); break;
+        }
+    }, [props.history.location.pathname]);
+
+    const goToHome = () => props.history.push('/');
+    // @TODOs
+    const goToBlog = () => props.history.push('/');
+    const goToPremium = () => props.history.push('/');
 
     return (
         <AppBar position="sticky">
             <KebabMenu open={Boolean(menuAnchor)} anchor={menuAnchor} onClose={() => setMenuAnchor(null)} />
             <Toolbar className={classes.toolbar}>
-                <div onClick={onClickLogo} className={classes.logo}>
+                <div onClick={goToHome} className={classes.logo}>
                     <img src={`${logo}#svgView(preserveAspectRatio(xMaxYMax))`} height="55px" style={{ cursor: 'pointer' }} />
                 </div>
-                <Typography className={classes.title2} variant="h5" color="inherit" style={{ cursor: 'pointer' }}>
-                    Find a mate
-                </Typography>
-                <Typography className={classes.title2} variant="h5" color="inherit">
-                    |
-                </Typography>
-                <Typography className={classes.title2} variant="h5" color="inherit" style={{ cursor: 'pointer' }}>
-                    Blog
-                </Typography>
-                <Typography className={classes.title2} variant="h5" color="inherit">
-                    |
-                </Typography>
-                <Typography className={classes.title} variant="h5" color="inherit" style={{ cursor: 'pointer' }}>
-                    Premium
-                </Typography>
-                <IconButton onClick={onClickLogo} color="inherit">
-                    <SearchIcon />
-                </IconButton>
-                <IconButton onClick={onClickLogo} color="inherit">
+                <div className={classes.navigation}>
+                    <Link className={classes.title} variant="h5" color="inherit" onClick={goToHome} style={navigationSelectedStyle.search}>
+                        Find a mate
+                    </Link>
+                    <Typography className={classes.title2} variant="h5" color="inherit">
+                        |
+                    </Typography>
+                    <Link className={classes.title} variant="h5" color="inherit" onClick={goToBlog} style={navigationSelectedStyle.blog}>
+                        Blog
+                    </Link>
+                    <Typography className={classes.title2} variant="h5" color="inherit">
+                        |
+                    </Typography>
+                    <Link className={classes.title} variant="h5" color="inherit" onClick={goToPremium} style={navigationSelectedStyle.premium}>
+                        Premium
+                    </Link>
+                </div>
+                <IconButton onClick={goToHome} color="inherit">
                     <ChatBubbleOutlineIcon />
                 </IconButton>
                 <IconButton onClick={(event) => setMenuAnchor(event.currentTarget)} color="inherit">
