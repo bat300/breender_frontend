373eadd2e159e1b0c1befae44cf732a714354613
Tatiana Baklanova <tatiana.baklanova@tum.de>
2021-07-14 22:16:27 +0200

    Adjust layout and add edit button

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -13,6 +13,8 @@ import EventIcon from '@material-ui/icons/Event';
 import VerificationIcon from '../VerificationIcon';
 import { Paper, Divider, Typography } from '@material-ui/core';
 import SearchResultElement from '../SearchResultElement';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,22 +22,13 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(1),
     },
     paper: {
+        backgroundColor: '#f7ebd7',
         padding: theme.spacing(4),
         paddingTop: theme.spacing(6),
         margin: 'auto',
         marginTop: theme.spacing(6),
         maxWidth: '85%',
     },
-    /*paper: {
-        padding: theme.spacing(2),
-        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-            marginTop: theme.spacing(6),
-            marginBottom: theme.spacing(6),
-            padding: theme.spacing(3),
-        },
-        maxWidth: '95%',
-        allign: 'center'
-    },*/
     paperSmall: {
         padding: theme.spacing(2),
         margin: 'auto',
@@ -51,73 +44,96 @@ const useStyles = makeStyles((theme) => ({
         margin: 'auto',
     },
     typography: {
-        padding: theme.spacing(2),
-        paddingTop: theme.spacing(6),
-        margin: 'auto',
-    }
+        margin: theme.spacing(2),
+        marginTop: theme.spacing(6),
+    },
+    divider: {
+        margin: theme.spacing(3),
+    },
+    black: {
+        color: '#fff',
+        backgroundColor: '#787878',
+    },
+    listItem: {
+        paddingLeft: '15%',
+        paddingRight: '15%',
+    },
+    button: {
+        margin: theme.spacing(1),
+    },
 }));
 
 export default function UserProfile(props) {
     const classes = useStyles();
 
     return (<div className={classes.root}>
-        <Paper className={classes.paper}>
-            <Typography variant="h6" align="center" style={{ fontWeight: 600 }}>
-                Account information
-            </Typography>
+        <Paper className={classes.paper} >
+            <Grid container justify="space-between" spacing={2}>
+                <Grid item />
+                <Grid item >
+                    <Typography variant="h6" align="center" style={{ fontWeight: 600 }}>
+                        Account information
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Button className={classes.button} variant="contained" color="secondary">
+                        Edit
+                    </Button>
+                </Grid>
+            </Grid>
             <List>
-                <ListItem>
+                <ListItem className={classes.listItem}>
                     <ListItemAvatar>
-                        <Avatar>
+                        <Avatar className={classes.black}>
                             <AccountCircleIcon />
                         </Avatar>
                     </ListItemAvatar>
                     <ListItemText primary={props.user.id} secondary="ID" />
                 </ListItem>
-                <ListItem>
+                <ListItem className={classes.listItem}>
                     <ListItemAvatar>
-                        <Avatar>
+                        <Avatar className={classes.black}>
                             <AccountCircleIcon />
                         </Avatar>
                     </ListItemAvatar>
                     <ListItemText primary={props.user.username} secondary="Username" />
                 </ListItem>
-                <ListItem>
+                <ListItem className={classes.listItem}>
                     <ListItemAvatar>
-                        <Avatar>
+                        <Avatar className={classes.black}>
                             <AlternateEmailIcon />
                         </Avatar>
                     </ListItemAvatar>
                     <ListItemText primary={props.user.email} secondary="Email" />
                     <VerificationIcon verified={props.user.isVerified} />
                 </ListItem>
-                <ListItem>
+                <ListItem className={classes.listItem}>
                     <ListItemAvatar>
-                        <Avatar>
+                        <Avatar className={classes.black}>
                             <LocationCityIcon />
                         </Avatar>
                     </ListItemAvatar>
                     <ListItemText primary={props.user.city} secondary="City" />
                 </ListItem>
-                <ListItem>
+                <ListItem className={classes.listItem}>
                     <ListItemAvatar>
-                        <Avatar>
+                        <Avatar className={classes.black}>
                             <AttachMoneyIcon />
                         </Avatar>
                     </ListItemAvatar>
                     <ListItemText primary={props.user.subscriptionPlan} secondary="Subscription plan" />
                 </ListItem>
                 {props.user.subscriptionPlan === 'premium' &&
-                    <ListItem>
+                    <ListItem className={classes.listItem}>
                         <ListItemAvatar>
-                            <Avatar>
+                            <Avatar className={classes.black}>
                                 <EventIcon />
                             </Avatar>
                         </ListItemAvatar>
                         <ListItemText primary={props.user.nextRenewalDate} secondary="Next renewal date" />
                     </ListItem>}
             </List>
-            <Divider variant="middle" className={classes.spacedDivider} />
+            <Divider variant="middle" className={classes.divider} />
             <Typography className={classes.typography} variant="h6" align="center" style={{ fontWeight: 600 }}>
                 Payment methods
             </Typography>
@@ -127,7 +143,7 @@ export default function UserProfile(props) {
                     <Typography> Details: {method.details}</Typography>
                 </Paper>) : <Typography className={classes.typographyNotifications}> No payment methods added yet</Typography>}
             </List >
-            <Divider variant="middle" className={classes.spacedDivider} />
+            <Divider variant="middle" className={classes.divider} />
             <Typography className={classes.typography} variant="h6" align="center" style={{ fontWeight: 600 }}>
                 My pets
             </Typography>
@@ -135,5 +151,5 @@ export default function UserProfile(props) {
                 {props.pets && props.pets.length > 0 ? props.pets.map((pet) => <SearchResultElement pet={pet} key={pet._id} />) : <Typography className={classes.typographyNotifications} align="center"> No pets added yet</Typography>}
             </List>
         </Paper>
-    </div>)
+    </div >)
 }
