e3d3a622bbb57b787b99cb7e27ad23652442bf25
Alina Turbina <alina.turbina@tum.de>
2021-07-15 10:40:38 +0200

    add redirect function, improve code, improve styles

--- src/components/pet-profile/PetPreviewProfileComponent.js
+++ src/components/pet-profile/PetPreviewProfileComponent.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Avatar, Grid, makeStyles, Typography } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 const PetPreviewProfileComponent = (props) => {
     const classes = useStyles();
+    const history = useHistory();
+
+    const { pet, username } = props;
+    const image = pet.profilePicture.src;
+
+    const redirectToPetPage = () => history.push(`/pet/${pet._id}`)
+
     return (
+        <div onClick={redirectToPetPage}>
         <Grid container className={classes.layout} spacing={2}>
             <Grid item>
-                <Avatar className={classes.picture} src={props.image} alt="Pet Profile Picture" />
+                <Avatar className={classes.picture} src={image} alt="Pet Profile Picture" />
             </Grid>
             <Grid item direction="column" spacing={2} className={classes.textLayout}>
-                <Typography variant="caption">{props.petsName}</Typography>
-                <Typography variant="caption">{props.username}</Typography>
+                <Typography variant="caption">{pet.officialName}</Typography>
+                <Typography variant="caption">{username}</Typography>
             </Grid>
         </Grid>
+        </div>
     );
 };
 
@@ -22,6 +32,7 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'row',
         justifyContent: 'left',
         alignItems: 'center',
+        cursor: 'pointer',
     },
     textLayout: {
         display: 'flex',
@@ -32,11 +43,6 @@ const useStyles = makeStyles((theme) => ({
     picture: {
         width: 50,
         height: 50,
-    },
-    modal: {
-        width: '80vw',
-        margin: '0 auto',
-        marginTop: 100,
     },
 }));
 
