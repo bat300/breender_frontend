383a3822c797536452d8d96330ca91b04ec9311a
Nikol Nikolova <nikol.nikolova@tum.de>
2021-07-24 00:01:13 +0200

    Add dummy blog posts and styling

--- src/components/blog/BlogComponent.js
+++ src/components/blog/BlogComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
+import PostComponent from './PostComponent';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper, Divider, Typography, Button } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { useHistory, withRouter } from 'react-router-dom';
 // state imports
 import { useUser } from 'helper/hooks';
 
@@ -18,50 +17,27 @@ function BlogComponent(props) {
     return (
         <div className={classes.layout}>
             <Grid container>
-                <Grid item className={classes.maxWidth}>
-                    <Paper className={classes.paper}>
-                        <Grid container alignItems="stretch" align="center" justify="center" direction="row" className={classes.gridMargin} spacing={2}></Grid>
-                    </Paper>
-                </Grid>
+                {props.posts.map((post) => {
+                    return (
+                        <Grid item className={classes.spaced}>
+                            <PostComponent post={post} />{' '}
+                        </Grid>
+                    );
+                })}
             </Grid>
         </div>
     );
 }
 
-export default withRouter(BlogComponent);
+export default BlogComponent;
 
 const useStyles = makeStyles((theme) => ({
     layout: {
         width: '80%',
         alignSelf: 'center',
     },
-    maxWidth: {
+    spaced: {
         width: '100%',
-    },
-    paper: {
         padding: theme.spacing(2),
-        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-            marginTop: theme.spacing(6),
-            marginBottom: theme.spacing(6),
-            padding: theme.spacing(3),
-        },
-    },
-    cards: {
-        margin: theme.spacing(2),
-    },
-    leftTypography: {
-        width: '100%',
-        alignItems: 'start',
-        marginBottom: theme.spacing(1),
-        marginLeft: theme.spacing(4),
-    },
-    gridMargin: {
-        marginTop: theme.spacing(2),
-    },
-    buttonGrid: {
-        marginBottom: 100,
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
     },
 }));
